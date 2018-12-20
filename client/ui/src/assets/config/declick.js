534cab4b330e1d9975c2526ab3144878ee5164f1
mathieuLauffenburger <mathieu.lauffenburger.dev@gmail.com>
2018-12-20 10:37:12 +0100

    yarn.lock && exemple.declick.js

--- client/ui/src/assets/config/declick.js
+++ client/ui/src/assets/config/declick.js
@@ -2,10 +2,10 @@ export default {
   domain: 'localhost',
   basePath: '/declick-ui/dist/',
   // apiUrl: 'http://www.declick.net/server/api/',
-  apiUrl: 'http://localhost/declick-server-v2/public/api/',
-  clientUrl: 'http://localhost/declick-client-v2/src/',
+  apiUrl: 'http://localhost:8000/api/',
+  clientUrl: 'http://declick-client.localhost/',
   cmsUrl: 'http://v2.declick.net/cms/',
   forumUrl: 'http://test.declick.net/forum',
   wikiUrl: 'http://create.declick.net/wiki/',
-  googleId: 'UA-9457907-8'
+  googleId: false
 }
---
+++ client/ui/src/assets/config/exemple.declick.js
@@ -0,0 +1,12 @@
+export default {
+    domain: 'localhost',
+    basePath: '/declick-ui/dist/',
+    // apiUrl: 'http://www.declick.net/server/api/',
+    apiUrl: 'http://localhost:8000/api/',
+    clientUrl: 'http://declick-client.localhost/',
+    cmsUrl: 'http://v2.declick.net/cms/',
+    forumUrl: 'http://test.declick.net/forum',
+    wikiUrl: 'http://create.declick.net/wiki/',
+    googleId: 'UA-9457907-8'
+  }
+  
