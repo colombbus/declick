1e65f4c0c9a3a5fc698e9dcb2a9f9c600d898318
mathieuLauffenburger <mathieu.lauffenburger.dev@gmail.com>
2019-04-15 10:40:24 +0200

    remove useless connect with google,fb

--- client/ui/src/components/user/AuthenticationModal.vue
+++ client/ui/src/components/user/AuthenticationModal.vue
@@ -37,18 +37,18 @@ transition(name='modal')
             span(v-show='isLogingIn')
               | &nbsp;
               span.fa.fa-circle-o-notch.fa-spin
-          button(
-            type='button'
-            class='btn btn-block btn-social btn-google disabled'
-          )
-            span.fa.fa-google
-            | se connecter via Google
-          button(
-            type='button'
-            class='btn btn-block btn-social btn-facebook disabled'
-          )
-            span.fa.fa-facebook
-            | se connecter via Facebook
+          // button(
+          //   type='button'
+          //   class='btn btn-block btn-social btn-google disabled'
+          // )
+          //   span.fa.fa-google
+          //   | se connecter via Google
+          // button(
+          //   type='button'
+          //   class='btn btn-block btn-social btn-facebook disabled'
+          // )
+          //   span.fa.fa-facebook
+          //   | se connecter via Facebook
           button(
             @click="mode = 'registration'"
             type='button'
