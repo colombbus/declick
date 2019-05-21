10e0aef203379cd4779bc079b48a28fb57b168c9
mathieuLauffenburger <mathieu.lauffenburger.dev@gmail.com>
2019-05-21 12:38:15 +0200

    add 404 page

---
+++ client/ui/src/components/page404.vue
@@ -0,0 +1,30 @@
+<template>
+  <div><img src="../assets/404.png" alt=""></div>
+</template>
+<script>
+export default {
+    metaInfo: () => ({
+    title: 'Declick',
+    titleTemplate: '%s ',
+    meta: [
+      { name: 'description', content: 'page 404'}
+    ],
+  }),
+}
+</script>
+<style scoped>
+  div{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+div img{
+  width: 50vw;
+  margin-top: 50px
+}
+@media only screen and (max-width: 576px){
+  div img{
+  width: 80vw;
+  }
+}
+</style>
