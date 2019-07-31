b7501faf755fc9858ed63ccd368560e9d6af85c0
bonpra <benoit.prady@colombbus.org>
2019-07-31 09:10:51 +0200

    Add first implementation of robot

---
+++ objects/src/classes/robot.js
@@ -0,0 +1,52 @@
+import Sprite from './sprite'
+import i18n from 'es2015-i18n-tag'
+import 'reflect-metadata'
+
+const DEFAULT_DISTANCE = 50
+
+@Reflect.metadata('translated', i18n`Robot`)
+class Robot extends Sprite {
+  constructor() {
+    super()
+    this._distance = DEFAULT_DISTANCE
+    this.addListener('movementChange', () => {
+      if (this._movement === 'stop') {
+        this._runtime.resume()
+      }
+    })
+  }
+
+  moveForward(steps) {
+    if (steps == null) {
+      steps = 1
+    }
+    super.moveForward(steps * this._distance)
+    this._runtime.suspend()
+  }
+
+  moveBackward(steps) {
+    if (steps == null) {
+      steps = 1
+    }
+    super.moveBackward(steps * this._distance)
+    this._runtime.suspend()
+  }
+
+  moveUpward(steps) {
+    if (steps == null) {
+      steps = 1
+    }
+    super.moveUpward(steps * this._distance)
+    this._runtime.suspend()
+  }
+
+  moveDownward(steps) {
+    if (steps == null) {
+      steps = 1
+    }
+    super.moveDownward(steps * this._distance)
+    this._runtime.suspend()
+  }
+}
+
+export default Robot
