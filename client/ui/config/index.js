cc30298a30fec94170ce4b74c3279c8b1ce6376f
mathieuLauffenburger <mathieu.lauffenburger.dev@gmail.com>
2019-10-09 16:35:42 +0200

    fixe build trouble

--- client/ui/config/index.js
+++ client/ui/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
     assetsSubDirectory: "static",
     // v2.declick
     // assetsPublicPath: "/ui/dist/",
-    assetsPublicPath: "/client/ui/dist/",
+    assetsPublicPath: "/",
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
