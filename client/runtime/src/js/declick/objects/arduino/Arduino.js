cfa265ce17c1139a2ef334438ab713fa7f2e5c1e
mathieuLauffenburger <mathieu.lauffenburger.dev@gmail.com>
2019-10-09 16:36:21 +0200

    add style arduino console

--- client/runtime/src/js/declick/objects/arduino/Arduino.js
+++ client/runtime/src/js/declick/objects/arduino/Arduino.js
@@ -1159,12 +1159,25 @@ define([
   *
   * */
   Arduino.prototype._createLoggerToggle = function() {
-    var toggleLogButton = document.createElement('div')
-    toggleLogButton.id = 'toggleLogButton'
-    // style change if logs enable
+    var toggleLogButton = document.createElement('svg'),
+    svgPath = document.createElement('path')
+    // svgPath.d = 
+    // toggleLogButton.append('../../../../images/microchip-solid.svg')
+    toggleLogButton.id = 'toggleLogButton';
 
+    toggleLogButton.setAttribute('aria-hidde',"true")
+    toggleLogButton.setAttribute('focusable',"false")
+    toggleLogButton.setAttribute('data-prefix',"fas")
+    toggleLogButton.setAttribute('data-icon',"microchip")
+    toggleLogButton.setAttribute('class',"svg-inline--fa fa-microchip fa-w-16")
+    toggleLogButton.setAttribute('role',"img")
+    toggleLogButton.setAttribute('xmlns',"http://www.w3.org/2000/svg")
+    toggleLogButton.setAttribute('viewBox',"0 0 512 512")
+    // style change if logs enable
+    svgPath.setAttribute('d',"M416 48v416c0 26.51-21.49 48-48 48H144c-26.51 0-48-21.49-48-48V48c0-26.51 21.49-48 48-48h224c26.51 0 48 21.49 48 48zm96 58v12a6 6 0 0 1-6 6h-18v6a6 6 0 0 1-6 6h-42V88h42a6 6 0 0 1 6 6v6h18a6 6 0 0 1 6 6zm0 96v12a6 6 0 0 1-6 6h-18v6a6 6 0 0 1-6 6h-42v-48h42a6 6 0 0 1 6 6v6h18a6 6 0 0 1 6 6zm0 96v12a6 6 0 0 1-6 6h-18v6a6 6 0 0 1-6 6h-42v-48h42a6 6 0 0 1 6 6v6h18a6 6 0 0 1 6 6zm0 96v12a6 6 0 0 1-6 6h-18v6a6 6 0 0 1-6 6h-42v-48h42a6 6 0 0 1 6 6v6h18a6 6 0 0 1 6 6zM30 376h42v48H30a6 6 0 0 1-6-6v-6H6a6 6 0 0 1-6-6v-12a6 6 0 0 1 6-6h18v-6a6 6 0 0 1 6-6zm0-96h42v48H30a6 6 0 0 1-6-6v-6H6a6 6 0 0 1-6-6v-12a6 6 0 0 1 6-6h18v-6a6 6 0 0 1 6-6zm0-96h42v48H30a6 6 0 0 1-6-6v-6H6a6 6 0 0 1-6-6v-12a6 6 0 0 1 6-6h18v-6a6 6 0 0 1 6-6zm0-96h42v48H30a6 6 0 0 1-6-6v-6H6a6 6 0 0 1-6-6v-12a6 6 0 0 1 6-6h18v-6a6 6 0 0 1 6-6z")
     tcanvas_container.appendChild(toggleLogButton)
-    toggleLogButton.style.display = 'none'
+    toggleLogButton.appendChild(svgPath)
+    // toggleLogButton.style.display = 'none'
 
     toggleLogButton.addEventListener('click', function(){
       
