00d9f09e4b1ac8a643156a40cbf9132bdfb07651
bonpra <benoit.prady@colombbus.org>
2020-01-10 16:55:09 +0100

    Add turtle object

---
+++ objects/src/classes/turtle.js
@@ -0,0 +1,56 @@
+import Robot from './robot'
+import i18n from 'es2015-i18n-tag'
+import 'reflect-metadata'
+
+@Reflect.metadata('translated', i18n`Turtle`)
+class Turtle extends Robot {
+  constructor() {
+    super()
+    this._trace = false
+    this._previousX = 0
+    this._previousY = 0
+    this._centerX = 0
+    this._centerY = 0
+    this._renderer = this._graphics.getRenderer()
+  }
+  _onReady() {
+    super._onReady()
+    this._centerX = Math.round(this._object.width / 2)
+    this._centerY = Math.round(this._object.height / 2)
+  }
+
+  @Reflect.metadata('translated', i18n`traceOn`)
+  traceOn() {
+    this._whenReady(() => {
+      if (!this._trace) {
+        this._previousX = this._x
+        this._previousY = this._y
+        this.addListener('move', this._updateTrace)
+        this._trace = true
+      }
+    })
+  }
+
+  @Reflect.metadata('translated', i18n`traceOff`)
+  traceOff() {
+    this._whenReady(() => {
+      if (this._trace) {
+        this.removeListener('move', this._updateTrace)
+        this._trace = false
+      }
+    })
+  }
+
+  _updateTrace(x, y) {
+    this._renderer.moveTo(
+      this._previousX + this._centerX,
+      this._previousY + this._centerY,
+    )
+    this._renderer.lineStyle(1, 0x000000)
+    this._renderer.lineTo(x + this._centerX, y + this._centerY)
+    this._previousX = x
+    this._previousY = y
+  }
+}
+
+export default Turtle
