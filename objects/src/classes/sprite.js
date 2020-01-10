1aba92ee51417997edcafda027690a14e4d5aaba
bonpra <benoit.prady@colombbus.org>
2020-01-10 16:54:39 +0100

    Wait for texture to be loaded

--- objects/src/classes/sprite.js
+++ objects/src/classes/sprite.js
@@ -10,32 +10,34 @@ class Sprite extends GraphicClass {
   constructor() {
     super()
     let texture = new PIXI.BaseTexture(robotTexture)
-    this._spriteSheet = new PIXI.Spritesheet(texture, robotData)
-    this._spriteSheet.parse(() => {
-      this._object = new PIXI.AnimatedSprite(
-        this._spriteSheet.animations['robot_face'],
-      )
-      this._object.animationSpeed = 0.1
-      this._object.play()
-      this.addListener('movementChange', () => {
-        let animation
-        if (this._movement === 'target') {
-          animation = this._targetX > this._x ? 'robot_right' : 'robot_left'
-        } else {
-          const animations = {
-            stop: 'robot_face',
-            forward: 'robot_right',
-            backward: 'robot_left',
-            upward: 'robot_face',
-            downward: 'robot_face',
-          }
-          animation = animations[this._movement]
-        }
-        this._object.stop()
-        this._object.textures = this._spriteSheet.animations[animation]
+    texture.on('loaded', () => {
+      this._spriteSheet = new PIXI.Spritesheet(texture, robotData)
+      this._spriteSheet.parse(() => {
+        this._object = new PIXI.AnimatedSprite(
+          this._spriteSheet.animations['robot_face'],
+        )
+        this._object.animationSpeed = 0.1
         this._object.play()
+        this.addListener('movementChange', () => {
+          let animation
+          if (this._movement === 'target') {
+            animation = this._targetX > this._x ? 'robot_right' : 'robot_left'
+          } else {
+            const animations = {
+              stop: 'robot_face',
+              forward: 'robot_right',
+              backward: 'robot_left',
+              upward: 'robot_face',
+              downward: 'robot_face',
+            }
+            animation = animations[this._movement]
+          }
+          this._object.stop()
+          this._object.textures = this._spriteSheet.animations[animation]
+          this._object.play()
+        })
+        this.dispatch('objectReady')
       })
-      this.dispatch('objectReady')
     })
   }
 }
