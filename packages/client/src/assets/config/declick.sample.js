ce2fffbeb80e22807bcb880a0f0594fab707e301
Regiloup <regis.leloup@colombbus.org>
2020-04-06 18:17:39 +0200

    refactor: rename config file

---
+++ packages/client/src/assets/config/declick.sample.js
@@ -0,0 +1,10 @@
+export default {
+  domain: 'localhost',
+  basePath: '/url/dist/',
+  apiUrl: 'http://localhost:8000/api/v1/',
+  clientUrl: 'http://url-client.localhost/',
+  cmsUrl: 'http://cms.url.net/cms/',
+  forumUrl: 'http://test.url.net/forum',
+  wikiUrl: 'http://create.url.net/wiki/',
+  googleId: 'UA-0000000-0',
+}
