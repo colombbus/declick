b95eb0b33be0c699f2784f308ad088f5a37c55d0
Regiloup <regis.leloup@colombbus.org>
2020-05-22 23:31:16 +0200

    feat(client): add conditional favicon

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
-const webpack = require('webpack')
+// const path = require('path')
+// const webpack = require('webpack')
 
 module.exports = {
   chainWebpack: config => {
@@ -18,7 +18,19 @@ module.exports = {
       .plugin('html-template')
         .use(HtmlWebpackPlugin, [{
           template: 'public/index.html',
+          favicon:
+            process.env.NODE_ENV === 'production'
+            ? 'public/favicon-prod.ico'
+            : 'public/favicon-dev.ico'
         }])
       .end()
+      const svgRule = config.module.rule('svg')
+      svgRule.uses.clear()
+      svgRule
+        .use('babel-loader')
+          .loader('babel-loader')
+        .end()
+        .use('vue-svg-loader')
+          .loader('vue-svg-loader')
   },
 }
