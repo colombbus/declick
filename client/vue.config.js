4a4c636217219e42326d651b62c787b7ba5a345a
Regiloup <regis.leloup@colombbus.org>
2020-05-23 15:51:39 +0200

    feat(client): add SVG support/fix config in vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -27,10 +27,19 @@ module.exports = {
       const svgRule = config.module.rule('svg')
       svgRule.uses.clear()
       svgRule
-        .use('babel-loader')
-          .loader('babel-loader')
-        .end()
-        .use('vue-svg-loader')
+        .oneOf('inline')
+        .resourceQuery(/inline/)
+        // use('babel-loader')
+        //   .loader('babel-loader')
+          .use('vue-svg-loader')
           .loader('vue-svg-loader')
+        .end()
+      .end()
+         .oneOf('external')
+          .use('file-loader')
+          .loader('file-loader')
+          .options({
+            name: 'assets/[name].[hash:8].[ext]',
+          });
   },
 }
