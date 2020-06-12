a3fb6061ebc5335ac501def75e69a681474e9b14
Benoit <benoit.prady@pix.fr>
2020-06-12 22:10:11 +0200

    Allow to change robot texture

--- objects/src/classes/robot.js
+++ objects/src/classes/robot.js
@@ -6,46 +6,48 @@ const DEFAULT_DISTANCE = 50
 
 @Reflect.metadata('translated', i18n`Robot`)
 class Robot extends Sprite {
-  constructor() {
-    super()
+  constructor(texture) {
+    super(texture)
     this._distance = DEFAULT_DISTANCE
+    this._moving = false
     this.addListener('movementChange', movement => {
       if (movement === 'stop') {
+        this._moving = false
         this._runtime.resume()
       }
     })
   }
 
-  moveForward(steps) {
-    if (steps == null) {
-      steps = 1
+  moveForward(steps = 1) {
+    if (!this._moving) {
+      this._moving = true
+      super.moveForward(steps * this._distance)
+      this._runtime.suspend(true)
     }
-    super.moveForward(steps * this._distance)
-    this._runtime.suspend()
   }
 
-  moveBackward(steps) {
-    if (steps == null) {
-      steps = 1
+  moveBackward(steps = 1) {
+    if (!this._moving) {
+      this._moving = true
+      super.moveBackward(steps * this._distance)
+      this._runtime.suspend(true)
     }
-    super.moveBackward(steps * this._distance)
-    this._runtime.suspend()
   }
 
-  moveUpward(steps) {
-    if (steps == null) {
-      steps = 1
+  moveUpward(steps = 1) {
+    if (!this._moving) {
+      this._moving = true
+      super.moveUpward(steps * this._distance)
+      this._runtime.suspend(true)
     }
-    super.moveUpward(steps * this._distance)
-    this._runtime.suspend()
   }
 
-  moveDownward(steps) {
-    if (steps == null) {
-      steps = 1
+  moveDownward(steps = 1) {
+    if (!this._moving) {
+      this._moving = true
+      super.moveDownward(steps * this._distance)
+      this._runtime.suspend(true)
     }
-    super.moveDownward(steps * this._distance)
-    this._runtime.suspend()
   }
 
   setLocation(x, y) {
