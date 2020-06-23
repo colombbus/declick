8feae6d08bf0e15498d5a6884552eeebe4ddd739
Benoit <benoit.prady@pix.fr>
2020-06-23 16:57:13 +0200

    Reimplement Text using Phaser

--- objects/src/classes/text.js
+++ objects/src/classes/text.js
@@ -1,35 +1,34 @@
 import i18n from 'es2015-i18n-tag'
 import GraphicClass from '../graphic-class'
 import 'reflect-metadata'
-import * as PIXI from 'pixi.js'
 
 @Reflect.metadata('translated', i18n`Text`)
 class Text extends GraphicClass {
-  constructor() {
+  constructor(value) {
     super()
-    this._value = ''
-  }
-
-  _createObject() {
-    this._object = new PIXI.Text(this._value)
-    this.dispatch('objectReady')
+    this._value = value !== undefined ? value : ''
+    this._object = this._graphics
+      .getScene()
+      .add.text(0, 0, this._value, { color: '#000000' })
+    this._object.setOrigin(0)
   }
 
   @Reflect.metadata('translated', i18n`setText`)
   @Reflect.metadata('help', i18n`setText_help`)
   setText(value) {
     this._value = value
-    if (this._object == null) {
-      this._createObject()
-    } else {
-      this._object.text = value
-    }
+    this._object.setText(value)
   }
 
   @Reflect.metadata('translated', i18n`getText`)
   @Reflect.metadata('help', i18n`getText_help`)
   getText() {
     return this._value
+  }
+
+  setLocation(x, y) {
+    super.setLocation(x, y)
+    this._object.setPosition(x, y)
   }
 }
 
