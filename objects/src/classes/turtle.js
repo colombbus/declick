ce32fcb533a9e1981fd9d2d5bdc35f3e5283f256
Benoit <benoit.prady@pix.fr>
2020-06-26 19:57:14 +0200

    Add abiilty to generate texture for Turtle

--- objects/src/classes/turtle.js
+++ objects/src/classes/turtle.js
@@ -1,6 +1,7 @@
 import Robot from './robot'
 import i18n from 'es2015-i18n-tag'
 import 'reflect-metadata'
+import Phaser from 'Phaser'
 
 @Reflect.metadata('translated', i18n`Turtle`)
 class Turtle extends Robot {
@@ -11,8 +12,13 @@ class Turtle extends Robot {
     this._previousY = 0
     this._traceX = 0
     this._traceY = 0
+  }
+
+  _buildObject() {
+    this._path = new Phaser.Curves.Path()
     this._renderer = this._graphics.getScene().add.graphics()
     this._renderer.lineStyle(1, 0x000000, 1.0)
+    super._buildObject()
   }
 
   @Reflect.metadata('translated', i18n`traceOn`)
@@ -22,8 +28,7 @@ class Turtle extends Robot {
       this._previousX = this._object.x
       this._previousY = this._object.y
       this._trace = true
-      this._renderer.beginPath()
-      this._renderer.moveTo(
+      this._path.moveTo(
         this._object.x + this._traceX,
         this._object.y + this._traceY,
       )
@@ -42,13 +47,26 @@ class Turtle extends Robot {
   @Reflect.metadata('help', i18n`setTracerLocation_help`)
   setTracerLocation(x, y) {
     if (this.trace) {
-      this._renderer.moveTo(
+      this._path.moveTo(
         this._object.x - this._traceX + x,
         this._object.y - this._traceY + y,
       )
     }
     this._traceX = x
     this._traceY = y
+  }
+
+  @Reflect.metadata('translated', i18n`createTexture`)
+  @Reflect.metadata('help', i18n`createTexture_help`)
+  createTexture(name) {
+    const graphics = this._graphics
+      .getScene()
+      .make.graphics({ x: 0, y: 0, add: false })
+    const bounds = this._path.getBounds()
+    graphics.translateCanvas(-bounds.x, -bounds.y)
+    this._path.draw(graphics)
+    graphics.generateTexture(name, bounds.width, bounds.height + 1)
+    graphics.destroy()
   }
 
   tick(delta) {
@@ -58,24 +76,14 @@ class Turtle extends Robot {
         this._object.x !== this._previousX ||
         this._object.y !== this.previousY
       ) {
-        this._renderer.lineTo(
+        this._path.lineTo(
           this._object.x + this._traceX,
           this._object.y + this._traceY,
         )
-        this._renderer.strokePath()
+        this._renderer.clear()
+        this._path.draw(this._renderer)
       }
     }
-  }
-
-  _updateTrace(x, y) {
-    this._renderer.moveTo(
-      this._previousX + this._centerX,
-      this._previousY + this._centerY,
-    )
-    this._renderer.lineStyle(1, 0x000000)
-    this._renderer.lineTo(x + this._centerX, y + this._centerY)
-    this._previousX = x
-    this._previousY = y
   }
 }
 
