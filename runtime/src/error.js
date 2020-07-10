cad6d5238d52b4da6f0704642237192f1ba926e3
Benoit <benoit.prady@pix.fr>
2020-07-10 11:54:56 +0200

    Enhance error detection

--- runtime/src/error.js
+++ runtime/src/error.js
@@ -4,6 +4,8 @@ export default class {
   constructor(e, states) {
     this._message = e.toString()
     this._error = e
+    this._start = false
+    this._end = false
     if (states) {
       this._detectError(e, states)
     }
@@ -17,22 +19,36 @@ export default class {
       const lastState = states[states.length - 1]
       const node = lastState.node
       this._message = `${node.raw}`
-      try {
-        if (
-          lastState.func_ &&
-          lastState.func_.type === 'undefined' &&
-          node.type === 'CallExpression'
-        ) {
-          // method undefined
-          this._message = `${node.raw}\nLa méthode ${node.callee.property.name} n'existe pas pour l'objet ${node.callee.object.name} `
-        } else if (node.type === 'NewExpression') {
-          // class unknown
-          this._message = `${node.raw}\nImpossible de créer un object de type ${node.callee.name}`
+      if (node.loc) {
+        this._start = node.loc.start
+        this._end = node.loc.end
+      }
+      /*if (interpreter.stateStack.length > 0) {
+        state = interpreter.stateStack[0]
+        if (!state.node.loc || !state.node.loc.source) {
+          // no program associated: remove lines if any
+          error.setLines([])
         } else {
-          this._message = ''
+          error.setProgramName(state.node.loc.source)
+        }
+      }*/
+      this._message = node.raw ? `${node.raw}\n` : ''
+      try {
+        if (e instanceof ReferenceError) {
+          if (node.type === 'NewExpression') {
+            // class unknown
+            this._message += `Impossible de créer un objet de type ${node.callee.name}`
+          } else if (
+            lastState.func_ &&
+            lastState.func_.type === 'undefined' &&
+            node.type === 'CallExpression'
+          ) {
+            // method undefined
+            this._message += `La méthode ${node.callee.property.name} n'existe pas pour l'objet ${node.callee.object.name} `
+          }
         }
       } catch (e) {
-        this._message = `Erreur d'exécution : ${e.message}`
+        this._message += `Erreur d'exécution : ${e.message}`
       }
     }
   }
@@ -45,5 +61,13 @@ export default class {
 
   getError() {
     return this._error
+  }
+
+  getStart() {
+    return this._start
+  }
+
+  getEnd() {
+    return this._end
   }
 }
