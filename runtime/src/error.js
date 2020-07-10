92cb599c504afac22a8401dce06b0f2cdc3bf9ae
Benoit <benoit.prady@pix.fr>
2020-07-10 12:41:47 +0200

    Enhance error detection

--- runtime/src/error.js
+++ runtime/src/error.js
@@ -1,4 +1,36 @@
-//TODO: à compléter
+//TODO: i18n
+let _getMessage = function(error, lastState, node) {
+  try {
+    if (error instanceof ReferenceError) {
+      if (node.type === 'NewExpression') {
+        // class unknown
+        return `Impossible de créer un objet de type ${node.callee.name}`
+      } else if (node.type === 'CallExpression') {
+        // unknown function
+        return `La fonction ${node.callee.name} n'existe pas`
+      }
+    } else if (error instanceof TypeError) {
+      if (
+        lastState.func_ &&
+        lastState.func_.type === 'undefined' &&
+        node.type === 'CallExpression'
+      ) {
+        // unknown method
+        return `La méthode ${node.callee.property.name} n'existe pas pour l'objet ${node.callee.object.name}`
+      }
+    } else if (error instanceof SyntaxError) {
+      if (error.message.toLowerCase().includes('unterminated string')) {
+        // unterminated string
+        return 'Il manque un guillemet'
+      } else {
+        return 'Erreur de syntaxe'
+      }
+    }
+  } catch {
+    return `Erreur d'exécution : ${error.toString()}`
+  }
+  return error.toString()
+}
 
 export default class {
   constructor(e, states) {
@@ -13,12 +45,10 @@ export default class {
 
   setLines() {}
 
-  //TODO: i18n
-  _detectError(e, states) {
+  _detectError(error, states) {
     if (states.length > 0) {
       const lastState = states[states.length - 1]
       const node = lastState.node
-      this._message = `${node.raw}`
       if (node.loc) {
         this._start = node.loc.start
         this._end = node.loc.end
@@ -32,24 +62,8 @@ export default class {
           error.setProgramName(state.node.loc.source)
         }
       }*/
-      this._message = node.raw ? `${node.raw}\n` : ''
-      try {
-        if (e instanceof ReferenceError) {
-          if (node.type === 'NewExpression') {
-            // class unknown
-            this._message += `Impossible de créer un objet de type ${node.callee.name}`
-          } else if (
-            lastState.func_ &&
-            lastState.func_.type === 'undefined' &&
-            node.type === 'CallExpression'
-          ) {
-            // method undefined
-            this._message += `La méthode ${node.callee.property.name} n'existe pas pour l'objet ${node.callee.object.name} `
-          }
-        }
-      } catch (e) {
-        this._message += `Erreur d'exécution : ${e.message}`
-      }
+      this._message =
+        (node.raw ? `${node.raw}\n` : '') + _getMessage(error, lastState, node)
     }
   }
 
---
+++ runtime/test/error.spec.js
@@ -0,0 +1,96 @@
+/*eslint-env mocha */
+import { assert } from 'chai'
+import declickParser from '../src/parser'
+import Interpreter from '../src/interpreter'
+import DeclickError from '../src/error'
+
+describe('when detecting an error', () => {
+  let interpreter
+
+  beforeEach(() => {
+    interpreter = new Interpreter('')
+  })
+
+  it('should detect instantiation of an unknown class', () => {
+    const code = `a = new Truc()`
+    const ast = declickParser.parse(code)
+    interpreter.appendCode(ast)
+    let error
+    try {
+      interpreter.run()
+    } catch (e) {
+      error = new DeclickError(e, interpreter.stateStack)
+    }
+    assert.equal(
+      error.getMessage(),
+      'new Truc()\nImpossible de créer un objet de type Truc',
+    )
+  })
+
+  it('should detect use of an unknown method', () => {
+    const code = `
+    a = new String('abcdef')
+    a.truc()
+    `
+    const ast = declickParser.parse(code)
+    interpreter.appendCode(ast)
+    let error
+    try {
+      interpreter.run()
+    } catch (e) {
+      error = new DeclickError(e, interpreter.stateStack)
+    }
+    assert.equal(
+      error.getMessage(),
+      "a.truc()\nLa méthode truc n'existe pas pour l'objet a",
+    )
+  })
+
+  it('should detect use of an unknown function', () => {
+    const code = `
+    truc()
+    `
+    const ast = declickParser.parse(code)
+    interpreter.appendCode(ast)
+    let error
+    try {
+      interpreter.run()
+    } catch (e) {
+      error = new DeclickError(e, interpreter.stateStack)
+    }
+    assert.equal(error.getMessage(), "truc()\nLa fonction truc n'existe pas")
+  })
+
+  it('should detect unterminated string', () => {
+    const code = `
+    a = 'coucou
+    b = 3 + 1
+    `
+    let ast
+    let error
+    try {
+      ast = declickParser.parse(code)
+      interpreter.appendCode(ast)
+      interpreter.run()
+    } catch (e) {
+      error = new DeclickError(e, interpreter.stateStack)
+    }
+    assert.equal(error.getMessage(), 'Il manque un guillemet')
+  })
+
+  it('should detect generic syntax error', () => {
+    const code = `
+    b = 3 +
+    `
+    let ast
+    let error
+    try {
+      ast = declickParser.parse(code)
+      interpreter.appendCode(ast)
+      interpreter.run()
+    } catch (e) {
+      error = new DeclickError(e, interpreter.stateStack)
+    }
+    assert.equal(error.getMessage(), 'Erreur de syntaxe')
+  })
+})
