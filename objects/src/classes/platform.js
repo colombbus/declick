89fead2cd4715e65d3fa86ba8ac2388dac9248d4
Benoit <benoit.prady@pix.fr>
2020-08-12 11:55:28 +0200

    Enhance collision and overlap mechanisms for platform

--- objects/src/classes/platform.js
+++ objects/src/classes/platform.js
@@ -37,13 +37,103 @@ class Platform extends GraphicClass {
       this._object.addTilesetImage(tileset.name, this._tiles),
     )
     this._layers = mapData.data.layers.map(layer => {
-      if (layer.dynamic !== undefined && layer.dynamic) {
+      const isDynamic =
+        (layer.dynamic !== undefined && layer.dynamic) ||
+        (layer.properties !== undefined &&
+          layer.properties.find(
+            property =>
+              (property.name === 'dynamic' ||
+                property.name === i18n`dynamic`) &&
+              property.value === true,
+          ) !== undefined)
+      if (isDynamic) {
         return this._object.createDynamicLayer(layer.name, tilesets)
       } else {
         return this._object.createStaticLayer(layer.name, tilesets)
       }
     })
-    this._object.setCollisionByProperty({ collides: true })
+
+    const ids = this._object.tilesets.reduce(
+      (tileIds, tileset) => {
+        for (
+          let i = tileset.firstgid;
+          i < tileset.firstgid + tileset.total;
+          i++
+        ) {
+          const properties = tileset.getTileProperties(i)
+          if (
+            properties.collides === true ||
+            properties[i18n`collides`] === true
+          ) {
+            tileIds[0].push(i)
+          }
+          if (
+            properties.overlaps === true ||
+            properties[i18n`overlaps`] === true
+          ) {
+            tileIds[1].push(i)
+          }
+        }
+        return tileIds
+      },
+      [[], []],
+    )
+
+    this._collisionTileIds = ids[0]
+    this._overlapTileIds = ids[1]
+
+    const interactiveLayers = this._layers.reduce(
+      (layers, layer) => {
+        let collision = false
+        let overlap = false
+        const properties = layer.layer.properties
+        properties.forEach(property => {
+          if (
+            property.name === 'collides' ||
+            property.name === i18n`collides`
+          ) {
+            collision = property.value
+          } else if (
+            property.name === 'overlaps' ||
+            property.name === i18n`overlaps`
+          ) {
+            overlap = property.value
+          }
+        })
+        if (!collision) {
+          if (
+            layer.filterTiles(tile =>
+              this._collisionTileIds.includes(tile.index),
+            ).length > 0
+          ) {
+            collision = true
+          }
+        }
+        if (!overlap) {
+          if (
+            layer.filterTiles(tile => this._overlapTileIds.includes(tile.index))
+              .length > 0
+          ) {
+            overlap = true
+          }
+        }
+        if (collision) {
+          layers[0].push(layer)
+        }
+        if (overlap) {
+          layers[1].push(layer)
+        }
+        return layers
+      },
+      [[], []],
+    )
+
+    this._collisionLayers = interactiveLayers[0]
+    this._overlapLayers = interactiveLayers[1]
+
+    this._collisionLayers.forEach(layer => {
+      layer.setCollision(this._collisionTileIds)
+    })
   }
 
   addCollider(object, handler) {
@@ -54,17 +144,25 @@ class Platform extends GraphicClass {
         handler(me, new PlatformTile(tile))
       }
     }
-    this._layers.forEach(layer => {
+    this._collisionLayers.forEach(layer => {
       scene.physics.add.collider(object, layer, collisionHandler)
     })
   }
 
   addOverlap(object, handler) {
-    //TODO: continue
-    /*const scene = this._graphics.getScene()
-    this._layers.forEach(layer => {
-      scene.physics.add.overlap(object, layer, handler)
-    })*/
+    const scene = this._graphics.getScene()
+    let overlapHandler
+    if (handler !== undefined) {
+      overlapHandler = (who, tile) => {
+        handler(who, new PlatformTile(tile))
+      }
+    }
+    this._overlapLayers.forEach(layer => {
+      if (overlapHandler) {
+        layer.setTileIndexCallback(this._overlapTileIds, overlapHandler, this)
+      }
+      scene.physics.add.overlap(object, layer)
+    })
   }
 
   @Reflect.metadata('translated', i18n`removeTile`)
