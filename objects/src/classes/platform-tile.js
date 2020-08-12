0e7f3487bf1b6e2774075b73fa7887cfe71cae36
Benoit <benoit.prady@pix.fr>
2020-08-12 12:07:24 +0200

    Add ability to set tile type

--- objects/src/classes/platform-tile.js
+++ objects/src/classes/platform-tile.js
@@ -1,6 +1,7 @@
 import i18n from 'es2015-i18n-tag'
 import BaseClass from '../base-class'
 import 'reflect-metadata'
+import { checkArguments } from '../utils'
 
 @Reflect.metadata('translated', i18n`PlatformTile`)
 class PlatformTile extends BaseClass {
@@ -23,6 +24,16 @@ class PlatformTile extends BaseClass {
 
   getY() {
     return this._tile.y
+  }
+
+  @Reflect.metadata('translated', i18n`is`)
+  @Reflect.metadata('help', i18n`is_help`)
+  @checkArguments(['string'])
+  is(value) {
+    return (
+      this._tile.properties.is === value ||
+      this._tile.properties[i18n`is`] === value
+    )
   }
 }
 
