ad1bc93582c61a1e4d8e8784d9356e316031d69e
Benoit <benoit.prady@pix.fr>
2020-08-13 17:41:23 +0200

    Update example tortue

--- demo/examples/tortue.js
+++ demo/examples/tortue.js
@@ -3,9 +3,9 @@ const resources = {
 }
 
 const code = `t = new Tortue('pen')
+t.définirPosition(2, 0)
 t.tracer()
 t.définirPositionTraceur(2, 72)
-t.définirPosition(1, 0)
 t.reculer()
 t.descendre()
 t.avancer()
@@ -26,6 +26,5 @@ t.avancer()
 t.descendre()
 t.créerTexture('copy')
 s = new Sprite('copy')
-s.définirPosition(400, 100)
-`
+s.définirPosition(50, 300)`
 export { resources, code }
