c40fb9c93c0308b48954f2adca78d2fdb38972c7
Benoit <benoit.prady@pix.fr>
2020-08-13 17:28:18 +0200

    Fix Camera to work even if graphics is re-created

--- objects/src/instances/camera.js
+++ objects/src/instances/camera.js
@@ -5,26 +5,24 @@ import { checkArguments } from '../utils'
 
 @Reflect.metadata('translated', i18n`camera`)
 class CameraClass extends BaseInstance {
-  constructor() {
-    super()
-    this._camera = null
-    this.addListener('runtimeInitialized', () => {
-      this._graphics = this._runtime.getGraphics()
-      this._camera = this._graphics.getScene().cameras.main
-    })
+  _getCamera() {
+    const graphics = this._runtime.getGraphics()
+    return graphics.getScene().cameras.main
   }
 
   @Reflect.metadata('translated', i18n`follow`)
   @Reflect.metadata('help', i18n`follow_help`)
   @checkArguments(['object'])
   follow(object) {
-    this._camera.startFollow(object.getGraphicalObject())
+    const camera = this._getCamera()
+    camera.startFollow(object.getGraphicalObject())
   }
 
   @Reflect.metadata('translated', i18n`stopFollowing`)
   @Reflect.metadata('help', i18n`stopFollowing_help`)
   stopFollowing() {
-    this._camera.stopFollow()
+    const camera = this._getCamera()
+    camera.stopFollow()
   }
 }
 export default CameraClass
