122d21996a8e6c46869aee3df81a7b9ee5be66e4
Regiloup <regis.leloup@colombbus.org>
2021-06-28 17:19:35 +0200

    add lmsUrl & blogToken to declick config file

--- client/src/assets/js/example.config.js
+++ client/src/assets/js/example.config.js
@@ -3,8 +3,11 @@ export default {
   basePath: '/declick-ui/dist/',
   apiUrl: 'http://localhost:8000/api/v1/',
   clientUrl: 'http://declick-client.localhost/',
+  lmsUrl: 'https://lms.moodle.url/',
   cmsUrl: 'http://v2.declick.net/cms/',
   forumUrl: 'http://test.declick.net/forum',
   wikiUrl: 'http://create.declick.net/wiki/',
   seoId: 'UA-XXXXXXX-X',
+  blogToken: '00000000000000000000000000000000',
+  debug: true,
 }
